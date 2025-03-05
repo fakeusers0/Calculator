3cf234117ffa5a31b09c54987aec4807e62aa573
fakeusers0 <fo2781831@gmail.com>
2025-03-05 11:18:12 +0530

    Update test_calculator.py

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -13,8 +13,8 @@ def test_multiply():
 def test_divide():
     assert Calculator.divide(6, 3) == 2
 
-def test_new():
-    assert Calculator.new(4,2) == True
+def test_even():
+    assert Calculator.even(4,2) == True
     
 def test_divide_by_zero():
     with pytest.raises(ValueError):
